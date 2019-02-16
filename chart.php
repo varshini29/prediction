5cb44aa26f1d18e9542acb21d4f5e812b0d35312
varshini29 <varshini55@live.com>
2019-02-16 21:55:50 +0400

    16.02.2019.2 - Tested chartjs and last 24 hours data working

--- chart.php
+++ chart.php
@@ -1,33 +1,21 @@
 <?php
 	include ('dbconnect.php');
 	include ('date.php');
-	
-	
-	 getLastDay();
-                        
-    function getLastDay() {
-		$rainfall = '';
-		$time = '';
-		$waterlevel='';
-		global $conn;
-                                        
-        $starting_day = getPreviousDays('1 day ago'); // 29/01/2019 14:48
-        $ending_day = getCurrentDate(); // 30/01/2019 14:48
-	//query to get data from the table
-	$sql = "SELECT rainfall_intensity, forecast_time, water_level FROM rainfall WHERE date BETWEEN '$starting_day'AND '$ending_day' AND status = 'inactive'AND drain_id = 1 ORDER BY forecast_time ";
-    $result = mysqli_query($conn, $sql);
 
-	//loop through the returned data
-	while ($row = mysqli_fetch_array($result)) {
-
-		$rainfall = $rainfall . '"'. $row['rainfall_intensity'].'",';
-		$time = $time . '"'. $row['forecast_time'] .'",';
-		$waterlevel = $waterlevel . '"'. $row['water_level'] .'",';
-	}
-
-	$rainfall = trim($rainfall,",");
-	$time = trim($time,",");
-	$waterlevel = trim($waterlevel,",");
+/**
+ * @description Getting last 24 hours data from MySQL based on drain id
+ * @param int $drainid
+ * @return array $result
+ */
+function getData($drainid) {
+	global $conn;
+	$starting_day = getPreviousDays('1 day ago');
+	$ending_day = getCurrentDate();
+	print_r($starting_day);
+	print_r($ending_day);
+	$sql = "SELECT rainfall_intensity, forecast_time, water_level, date FROM rainfall WHERE date BETWEEN '$starting_day'AND '$ending_day' AND status = 'inactive'AND drain_id = $drainid ORDER BY forecast_time ";
+	$result = mysqli_query($conn, $sql);
+	return $result;
 }
 ?>
 
@@ -62,6 +50,21 @@
 	    <div class="container">	
        
 			<canvas id="chart" style="width: 500px; height: 50px; background: #222; border: 1px solid #555652; margin-top: 10px;"></canvas>
+			<?php 
+			
+			$rainfall = '';
+			$time = '';
+			$waterlevel='';
+
+			// Pass the drain id to get data from. E.g getData(2) will get data from drain id 2.
+			$result = getData(1);
+
+			while ($row = mysqli_fetch_array($result)) {
+				$rainfall = $rainfall . '"'. $row['rainfall_intensity'].'",';
+				$time = $time . '"'. $row['forecast_time'] .'",';
+				$waterlevel = $waterlevel . '"'. $row['water_level'] .'",';
+			} 
+			?>
 
 			<script>
 				var ctx = document.getElementById("chart").getContext('2d');
