c143bb0182fd7f204fd72a89907599f2586fb696
varshini29 <varshini55@live.com>
2019-03-07 19:14:03 +0400

    07.02.2019 - Fixed footer in some files

--- last24.php
+++ last24.php
@@ -378,9 +378,13 @@ function getData($drainid) {
 			</main> <!-- .main-content -->
 
 			<footer class="site-footer">
-				<div class="container">
-				<p class="colophon">Copyright 2019 University of Mauritius. All rights reserved</p>
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-lg-12 my-2">
+						<p class="m-0 text-center">&copy; 2019 University of Mauritius. All rights reserved</p>
+					</div>
 				</div>
+			</div>
 				</footer> <!-- .site-footer -->
 		</div>
 		
